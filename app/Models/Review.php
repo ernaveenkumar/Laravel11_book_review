141ce90c3fbf12787abcb33bd395e479e9658ade
Naveen Kumar <er.crackerjack@gmail.com>
2024-10-21 14:37:59 +1300

    Invalidate cache functionality added in Review Model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -15,4 +15,12 @@ class Review extends Model
 
         return $this->belongsTo(Book::class);
     }
+
+    protected static function booted()
+    {
+        //Whenever the Review model is modified below code will be triggered
+        static::updated((fn (Review $review)=> cache()->forget('book:'. $review->book_id)));
+
+        static::deleted(fn(Review $review) => cache()->forget('book:' . $review->book_id));
+    }
 }
